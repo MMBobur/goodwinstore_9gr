34a93aadbee57a2b5d42a6bfd7b4689d446d75fc
Umidjon <umidjonshodiyorov@gmail.com>
2022-06-22 23:43:17 -0700

    'asasas'

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Navbar from "./../../components/navbar"
+import Lates from "./../../components/lates"
 
 function Home() {
   return (
     <div>
       <Navbar/>
+      <Lates/>
     </div>
   )
 }
