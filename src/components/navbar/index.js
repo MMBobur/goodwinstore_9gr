020efedba92b2fc184c34d64c0bcc2b9141688ae
Umidjon <umidjonshodiyorov@gmail.com>
2022-06-23 06:53:07 -0700

    'navbar'

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -14,9 +14,31 @@ import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import Logo from './../../assets/logo.png'
 import Icon from './../../assets/shopIcon.png'
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 
-const pages = ['Layouts', 'SHop buy', 'Clothng','Footwer','Sale','Buy Theme'];
+const pages = ['Layouts', 'SHop buy', 'Clothng', 'Footwer', 'Sale', 'Buy Theme'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const dropdown = [
+  <Box>
+    <PopupState variant="popover" popupId="demo-popup-menu">
+      {(popupState) => (
+        <React.Fragment>
+          <Button {...bindTrigger(popupState)}>
+            LAYOUTS
+          </Button>
+          <Menu {...bindMenu(popupState)} sx={{ marginLeft: '8.5%', marginTop: '-2.5%' }}>
+            <MenuItem onClick={popupState.close}>Profile</MenuItem>
+            <MenuItem onClick={popupState.close}>My account</MenuItem>
+            <MenuItem onClick={popupState.close}>Logout</MenuItem>
+          </Menu>
+        </React.Fragment>
+      )}
+    </PopupState>
+  </Box>
+]
 
 const ResponsiveAppBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -36,10 +58,29 @@ const ResponsiveAppBar = () => {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
 
   return (
     <AppBar position="static">
-      <Container maxWidth="xl" sx={{height:'12vh',bgcolor:'white',justifyContent:'center',textAlign:'center'}}>
+      <Box sx={{ bgcolor: '#9AC3E1', height: '10vh' }}>
+        <Stack direction="row" justifyContent='center' spacing={2} sx={{ bgcolor: '#9AC3E1', display: 'flex', gap: '5vh', paddingTop: '2vh' }}>
+          <Item sx={{ bgcolor: '#9AC3E1', color: 'white', boxShadow: 'none' }}>eng</Item>
+          <Item sx={{ bgcolor: '#9AC3E1', color: 'white', boxShadow: 'none' }}>usd</Item>
+          <Item sx={{ bgcolor: '#9AC3E1', color: 'white', boxShadow: 'none', fontSize: '12px' }}> STANDARD DELIVERY ON ORDERS OVER $ 150</Item>
+          <Item sx={{ bgcolor: '#9AC3E1', color: 'white', boxShadow: 'none' }}>8 800 265 86 56</Item>
+          <Item sx={{ bgcolor: '#9AC3E1', color: 'white', boxShadow: 'none' }}>Search</Item>
+          <Item sx={{ bgcolor: '#9AC3E1', color: 'white', boxShadow: 'none' }}>Wishlist</Item>
+          <Item sx={{ bgcolor: '#9AC3E1', color: 'white', boxShadow: 'none' }}>My account</Item>
+        </Stack>
+      </Box>
+      <Container maxWidth="xl" sx={{ height: '12vh', bgcolor: 'white', justifyContent: 'center', textAlign: 'center' }}>
         <Toolbar disableGutters>
           <Typography> <img src={Logo} /></Typography>
           <Typography
@@ -57,7 +98,7 @@ const ResponsiveAppBar = () => {
               textDecoration: 'none',
             }}
           >
-            
+
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -115,16 +156,93 @@ const ResponsiveAppBar = () => {
           >
             LOGO
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' },justifyContent:'center' }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'black', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center' }}>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: 'black', display: 'block' }}
+            >
+              <PopupState variant="popover" popupId="demo-popup-menu">
+                {(popupState) => (
+                  <React.Fragment>
+                    <Button sx={{ color: 'black' }} {...bindTrigger(popupState)}>
+                      LAYOUTS
+                    </Button>
+                    <Menu {...bindMenu(popupState)}>
+                      <PopupState variant="popover" popupId="demo-popup-menu">
+                        {(popupState) => (
+                          <React.Fragment>
+                            <Button sx={{ color: 'black' }} {...bindTrigger(popupState)}>
+                              LAYOUTS
+                            </Button>
+                            <Menu {...bindMenu(popupState)} sx={{ marginLeft: '6.5%', marginTop: '-2.5%' }}>
+                              <MenuItem onClick={popupState.close}>Profile</MenuItem>
+                              <MenuItem onClick={popupState.close}>My account</MenuItem>
+                              <MenuItem onClick={popupState.close}>Logout</MenuItem>
+                            </Menu>
+                          </React.Fragment>
+                        )}
+                      </PopupState><br />
+                      <PopupState variant="popover" popupId="demo-popup-menu">
+                        {(popupState) => (
+                          <React.Fragment>
+                            <Button sx={{ color: 'black' }} {...bindTrigger(popupState)}>
+                              LAYOUTS
+                            </Button>
+                            <Menu {...bindMenu(popupState)} sx={{ marginLeft: '6.5%', marginTop: '-2.5%' }}>
+                              <MenuItem onClick={popupState.close}>Profile</MenuItem>
+                              <MenuItem onClick={popupState.close}>My account</MenuItem>
+                              <MenuItem onClick={popupState.close}>Logout</MenuItem>
+                            </Menu>
+                          </React.Fragment>
+                        )}
+                      </PopupState>
+                      <MenuItem onClick={popupState.close}></MenuItem>
+                    </Menu>
+                  </React.Fragment>
+                )}
+              </PopupState>
+            </Button>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: 'black', display: 'block' }}
+            >
+              <PopupState variant="popover" popupId="demo-popup-menu">
+                {(popupState) => (
+                  <React.Fragment>
+                    <Button sx={{ color: 'black' }} {...bindTrigger(popupState)}>
+                      SHOP BY
+                    </Button>
+                    <Menu {...bindMenu(popupState)}>
+                      <MenuItem onClick={popupState.close}>Profile</MenuItem>
+                      <MenuItem onClick={popupState.close}>My account</MenuItem>
+                      <MenuItem onClick={popupState.close}>Logout</MenuItem>
+                    </Menu>
+                  </React.Fragment>
+                )}
+              </PopupState>
+            </Button>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: 'black', display: 'block' }}
+            >
+              <PopupState variant="popover" popupId="demo-popup-menu">
+                {(popupState) => (
+                  <React.Fragment>
+                    <Button sx={{ color: 'black' }} {...bindTrigger(popupState)}>
+                      CLOTHING
+                    </Button>
+                    <Menu {...bindMenu(popupState)}>
+                      <MenuItem onClick={popupState.close}>Profile</MenuItem>
+                      <MenuItem onClick={popupState.close}>My account</MenuItem>
+                      <MenuItem onClick={popupState.close}>Logout</MenuItem>
+                    </Menu>
+                  </React.Fragment>
+                )}
+              </PopupState>
+            </Button>
+            <Button sx={{color:'black'}}>FOOTWEAR</Button>
+            <Button sx={{color:'black'}}>SALE</Button>
+            <Button sx={{color:'black'}} >BUY THEME <span style={{color:'red'}} >-20%</span></Button>
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
@@ -155,7 +273,7 @@ const ResponsiveAppBar = () => {
                 </MenuItem>
               ))}
             </Menu>
-            <Typography><img style={{fontSize:'5000px'}} src={Icon}/></Typography>
+            <Typography><img style={{ fontSize: '5000px' }} src={Icon} /></Typography>
           </Box>
         </Toolbar>
       </Container>
