99baf306e831c720a7350343018a1fc3cdc7404f
feruzbek006 <nomonjonovferuzbek006@gmail.com>
2022-06-23 11:18:47 +0500

    'asasasasa'

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Navbar from "./../../components/navbar"
+import Recommended from "../../components/recommended"
 
 function Home() {
   return (
     <div>
       <Navbar/>
+      <Recommended/>
     </div>
   )
 }
